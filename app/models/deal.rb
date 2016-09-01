145ff5117fb3ee572d10f32069926effb8eabd8d
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2016-09-01 23:26:34 +0100

    Change image sizes

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,6 @@
 class Deal < ApplicationRecord
   belongs_to :user
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", square: "300x300#"}
 
   do_not_validate_attachment_file_type :photo
   validates :title, presence: true
