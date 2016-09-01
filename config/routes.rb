8ddc60e17df2cafb22ab679e1d9b934a5fb4f9ca
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2016-09-01 22:00:11 +0100

    Add bootstrap and change app root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :deals
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'deals#index'
 end
