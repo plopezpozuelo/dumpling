77da82be1df50d72b21287f8cb111888f009f218
Paula Lopez Pozuelo <plopezpozuelo@gmail.com>
2016-09-04 22:13:50 +0100

    Another attempt to store images in s3 bucket

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
-# Paperclip::Attachment.default_options[:url] = 'http://many-non-linear-deals.s3-website-eu-west-1.amazonaws.com/'
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 # Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# Paperclip::Attachment.default_options[:s3_host_name] = 's3-website-eu-west-1.amazonaws.com'
